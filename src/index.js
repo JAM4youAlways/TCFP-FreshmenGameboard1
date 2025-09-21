6cec17a59f52e3b94347e55fbc05cec5a2b6e9ad
JAM4youAlways <jmckean@thecollegefamilyplan.com>
2025-09-21 09:04:49 -0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import FreshmanGameboard from './FreshmanGameboard';
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<FreshmanGameboard />);
